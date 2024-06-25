d5d8a2b37eba38e4e673ed0662eaf5fe980d30b8
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-06-25 22:19:40 +0300

    completes a challenging task

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+console.log(saturdayFun());
+console.log(saturdayFun("go hiking")); 
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+mondayWork();
+
+
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
